1f0398cb5ba90f61470cd5809c061f545de121b3
YoannPa <yoann.pageaud@gmail.com>
2020-03-10 16:44:49 +0100

    Small test script to create a hexsticker

--- Create_R_hexSticker.R
+++ Create_R_hexSticker.R
@@ -9,7 +9,20 @@ Author = 'Yoann PAGEAUD'
 Maintainer = 'Yoann PAGEAUD (yoann.pageaud@gmail.com)'
 Dependencies = c('R version 3.6.2 (2019-12-12)',
 'RStudio Version 1.2.5019 – © 2009-2019','hexSticker')
-Description = 'Create a hexSticker for a R package.'
+Description = 'Creates a hexSticker for a R package.'
 ################################################################################
 ");
 if (is.null(parse_args(opt_parser)$file) != TRUE){print_help(opt_parser);quit()}
+
+##IMPORTS
+setwd("/home/yoann/DTrsiv/")
+library(hexSticker)
+library(magick)
+imgurl<-system.file("img/DTrsiv_icon.png", package="DTrsiv")
+sticker(imgurl, package="DTrsiv", asp = 12, dpi = 600,
+        s_x = 0.95, s_y = 1,
+        p_x = 1, p_y = 0.75, p_color = "black", p_size = 27,
+        h_color = "yellow",
+        spotlight = TRUE, l_x = 0.98, l_y = 1, l_width = 6, l_height = 6,
+        l_alpha = 0.3,
+        filename="img/my_hexsticker.png")
