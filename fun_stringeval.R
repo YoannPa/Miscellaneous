bbaac8bf28c43194eb64b0e2b0c8ed3791a35125
YoannPa <yoann.pageaud@gmail.com>
2020-10-07 17:26:02 +0200

    New string evalutation function in R.

--- fun_stringeval.R
+++ fun_stringeval.R
@@ -8,7 +8,18 @@
 #' @value A \code{logical}.
 #' @author Yoann Pageaud.
 #' @export
-#' @examples
-#' @references
 
 is.upper<-function(str){ grepl(pattern = "[A-Z]+", x = str) }
+
+#' @description Check if an array, a dataframe or a matrix contains any empty
+#'              cell. 
+#' 
+#' @param data An \code{array}, a \code{dataframe} or a \code{matrix}.
+#' @return A \code{logical}, TRUE if the array contains at least 1 empty cell,
+#'         FALSE if it does not.
+#' @author Yoann Pageaud.
+#' @export
+
+any.empty <- function(data){
+  any(apply(X = data, MARGIN = 2,FUN = grepl, pattern = "^$"))
+}
