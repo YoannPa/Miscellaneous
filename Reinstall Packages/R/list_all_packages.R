9e7105a5590fa56c279867533e9c1bba8669f1d5
YoannPa <yoann.pageaud@gmail.com>
2023-05-05 16:02:48 +0200

    Add library path.

--- Reinstall Packages/R/list_all_packages.R
+++ Reinstall Packages/R/list_all_packages.R
@@ -14,5 +14,7 @@ Description = 'Print installed packages in a text file.'
 ");
 if (is.null(parse_args(opt_parser)$file) != TRUE){print_help(opt_parser);quit()}
 
-write.table(x = installed.packages()[,1], file = "List_R_Packages.txt",
-            row.names = FALSE, col.names = FALSE, quote = FALSE)
+write.table(
+  x = installed.packages(lib.loc = "~/R/x86_64-pc-linux-gnu-library/4.2/")[,1],
+  file = "List_R_Packages.txt", row.names = FALSE, col.names = FALSE,
+  quote = FALSE)
