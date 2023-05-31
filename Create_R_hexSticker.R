395c66cc4e6479d5cc3d70d9707ef15beba5496f
YoannPa <yoann.pageaud@gmail.com>
2023-05-31 20:27:40 +0200

    Update on R packages hexSticker creation with custom fonts

--- Create_R_hexSticker.R
+++ Create_R_hexSticker.R
@@ -20,6 +20,8 @@ if (is.null(parse_args(opt_parser)$file) != TRUE){print_help(opt_parser);quit()}
 # The purpose of this script is to provide some HexSticker creation examples.
 
 ##IMPORTS
+extrafont::loadfonts()
+library(ggplot2)
 library(hexSticker)
 
 ## methview.qc
@@ -31,14 +33,19 @@ sticker(subplot = "img/methview_logo_refined_final.png", package = "",
 
 ## NCBI.BLAST2DT
 setwd("~/NCBI.BLAST2DT/")
-sticker(subplot = "img/New Project.png", package = "NCBI.BLAST2DT",
-        dpi = 300, s_x = 1, s_y = 0.95, s_width = 1, p_y = 1.35, p_size = 18,
-        p_family = "Tiepolo Book SC", p_color = "#154a89", h_color = "#154a89",
-        h_fill = "#26bdb0", asp = 0.85, filename = "img/my_hexsticker.png")
+# hexSticker does not look for font in those loaded in R!
+# You need to manually copy the .ttf file in
+# /home/user/R/x86_64-pc-linux-gnu-library/4.2/hexSticker/fonts
+# Create hexsticker with custom font
+sticker(subplot = "img/Remake_without_background.png",
+        package = "NCBI.BLAST2DT", dpi = 300, s_x = 1, s_y = 1.2, s_width = 0.9,
+        p_y = 0.7, p_size = 18, p_family = "ITC Tiepolo W01 Book SC",
+        p_color = "#154a89", h_color = "#154a89", h_fill = "#26bdb0",
+        asp = 0.85, filename = "img/my_hexsticker.png")
 
 ## DTrsiv
 setwd("~/DTrsiv/")
-sticker(subplot = "img/DTrsiv_icon.png", package = "DTrsiv", asp = 12,
+sticker(subplot = "img/", package = "DTrsiv", asp = 12,
         dpi = 600, s_x = 0.95, s_y = 1, p_x = 1, p_y = 0.75, p_color = "black",
         p_size = 27, h_color = "yellow", spotlight = TRUE, l_x = 0.98, l_y = 1,
         l_width = 6, l_height = 6, l_alpha = 0.3,
